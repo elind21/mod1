92224747a2be383075790ce60bf8641e0af3ae95
elind21 <eriklindbergs@gmail.com>
2014-08-14 20:20:48 +1000

    Updated MCForge, fixed compat. with gradle wrapper.
    setup workspac environment

---
+++ src/main/java/com/elind21/mod1/mod1.java
@@ -0,0 +1,31 @@
+package com.elind21.mod1;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "mod1", name = "Mod1", version = "1.7.10-1.0")
+public class mod1
+{
+    @Mod.Instance("mod1")
+    public static mod1 instances;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
